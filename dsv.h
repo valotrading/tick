f759829b9b366ada3a024743cf45e25820dee822
Jussi Virtanen <jussi.k.virtanen@gmail.com>
2013-06-05 11:20:46 +0300

    Rename 'fmt_char' to 'dsv_fmt_char'
    Signed-off-by: Jussi Virtanen <jussi.k.virtanen@gmail.com>

--- dsv.h
+++ dsv.h
@@ -3,6 +3,16 @@
 
 #include <stdlib.h>
 
+static inline size_t dsv_fmt_char(char *buf, unsigned char c, char delim)
+{
+	size_t ret = 0;
+
+	buf[ret++] = c;
+	buf[ret++] = delim;
+
+	return ret;
+}
+
 void dsv_write_header(int fd, const char *columns[], size_t num_columns, char delim);
 
 #endif
--- ob.c
+++ ob.c
@@ -27,16 +27,6 @@ void ob_write_header(int fd)
 	dsv_write_header(fd, column_names, ARRAY_SIZE(column_names), '\t');
 }
 
-static size_t fmt_char(char *buf, unsigned char c, char sep)
-{
-	size_t ret = 0;
-
-	buf[ret++] = c;
-	buf[ret++] = sep;
-
-	return ret;
-}
-
 static size_t fmt_price(char *buf, struct ob_price *price, char sep)
 {
 	size_t ret = 0;
@@ -78,7 +68,7 @@ void ob_write_event(int fd, struct ob_event *event)
 	size_t idx = 0;
 	char buf[1024];
 
-	idx += fmt_char (buf + idx, event->type, '\t');
+	idx += dsv_fmt_char(buf + idx, event->type, '\t');
 	idx += fmt_value(buf + idx, event->date, event->date_len, '\t');
 	idx += fmt_value(buf + idx, event->time, event->time_len, '\t');
 	idx += fmt_value(buf + idx, event->time_zone, event->time_zone_len, '\t');
